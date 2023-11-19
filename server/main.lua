d46079fc3793b6143a1b14d4455a6d17c3af3977
HebiKotei <c1yvan@outlook.com>
2023-11-19 15:59:58 +0000

    feat: explicit function implementation

--- server/main.lua
+++ server/main.lua
@@ -29,7 +29,7 @@ exports('CreateEntity', createEntity)
 
 --- Fetches DB Vehicle Entity
 ---@param query FetchVehicleEntityQuery
----@return vehicleEntity[]
+---@return vehicleEntity[]|nil
 local function fetchEntity(query)
     if query.valueType ~= 'citizenid' and query.valueType ~= 'license' and query.valueType ~= 'plate' then return end
     return MySQL.await('SELECT * FROM player_vehicles WHERE ? = ?', {
@@ -57,10 +57,31 @@ end
 
 exports('UpdateEntity', updateEntity)
 
---- Deletes a DB Vehicle Entity
+---@param citizenId string
+---@param license string
 ---@param vehiclePlate string
-local function deleteEntity(vehiclePlate)
+local function updateEntityOwner(citizenId, license, vehiclePlate)
+    MySQL.update('UPDATE player_vehicles SET citizenid = ?, license = ? WHERE plate = ?', {
+        citizenId,
+        license,
+        vehiclePlate
+    })
+end
+
+exports("UpdateEntityOwner", updateEntityOwner)
+
+--- Deletes a DB Vehicle Entity through searching for the number plate
+---@param vehiclePlate string
+local function deleteEntityFromPlate(vehiclePlate)
     MySQL.query('DELETE FROM player_vehicles WHERE plate = ?', {vehiclePlate})
 end
 
-exports('DeleteEntity', deleteEntity)
+exports('DeleteEntityFromPlate', deleteEntityFromPlate)
+
+--- Deletes DB Vehicle entities(-y) through searching for the citizen id
+---@param citizenId string 
+local function deleteEntitiesByCitizenId(citizenId)
+    MySQL.query('DELETE FROM player_vehicels WHERE citizenid = ?', {citizenId})
+end
+
+exports('DeleteEntitiesByCitizenId', deleteEntitiesByCitizenId)
