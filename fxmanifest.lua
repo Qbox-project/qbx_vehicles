f5a594628c164534a5357cf26c503d698688d970
HebiKotei <c1yvan@outlook.com>
2023-11-19 12:09:02 +0000

    feat:Initial Commit + Command Structure

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fxversion 'cerulean'
+game 'gta5'
+
+description 'QBX_Vehicles'
+repository 'https://github.com/Qbox-project/qbx_vehicles'
+version '0.0.1'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+provide 'qb-vehicles'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
---
+++ server/main.lua
@@ -0,0 +1,31 @@
+--- Creates a Vehicle DB Entity
+---@return nil
+local function createEntity()
+    return
+end
+
+exports('CreateEntity', createEntity)
+
+--- Fetch Entity
+---@return nil
+local function fetchEntity()
+    return
+end
+
+exports('FetchEntity', fetchEntity)
+
+--- Updates a DB Entity
+---@return nil
+local function updateEntity()
+    return
+end
+
+exports('UdateEntity', updateEntity)
+
+--- Deletes a DB Entity
+---@return nil
+local function deleteEntity()
+    return
+end
+
+exports('DeleteEntity', deleteEntity)
